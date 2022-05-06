9cb97c553ab4c0528964c312f8fe38d1eca442f0
Gen Taliar <gen@infira.ee>
2022-05-06 11:46:50 +0300

    bash fixes

--- bash/dropLocalTables.sh
+++ bash/dropLocalTables.sh
@@ -1,16 +1,16 @@
-declare db=$1
-declare tempPath=$2
-# shellcheck disable=SC2006
-for table in $(echo "show tables" | sudo mysql ${db}); do
-  if [[ ! $table == "Tables_in_${db}" ]]; then
-    if [[ $table == "v_"* ]]; then
-      DROP_TABLES_STRING+=" drop view ${db}.${table};"
-    else
-      DROP_TABLES_STRING+=" drop table ${db}.${table};"
-    fi
+#!/bin/bash
+db=$1
+tempPath=$2
+lines=$(sudo mysql --skip-column-names -e"SHOW FULL tables FROM ${db}")
+while read -r table type; do
+  if [[ $type == "VIEW" ]]; then
+    DROP_TABLES_STRING+=" drop view ${db}.${table};"
+  else
+    DROP_TABLES_STRING+=" drop table ${db}.${table};"
   fi
-done
+done <<<"$lines"
+
 declare dropPath="${tempPath}${db}.drop.sql"
-echo "${DROP_TABLES_STRING}" >> ${dropPath}
-sudo mysql ${db} < ${dropPath}
+echo "${DROP_TABLES_STRING}" >>${dropPath}
+sudo mysql ${db} <${dropPath}
 rm -f ${dropPath}
